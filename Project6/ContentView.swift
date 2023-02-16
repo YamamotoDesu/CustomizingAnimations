a0c839b7bb09fce9622c21381a134c1608bf78e4
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-02-16 23:01:50 +0900

    Initial Commit

---
+++ Project6/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Project6
+//
+//  Created by Paul Hudson on 17/02/2020.
+//  Copyright © 2020 Paul Hudson. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var animationAmount: CGFloat = 1
+
+    var body: some View {
+        Button("Tap Me") {
+            self.animationAmount += 1
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
